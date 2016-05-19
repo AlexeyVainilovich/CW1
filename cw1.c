7cc2569f8a88c889ee5ab579c602b05f1fef689d
AlexeyVainilovich <Alexey.Vainilovich@mail.ru>
2016-05-19 08:31:57 +0300

    Add files via upload

--- cw1.c
+++ cw1.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
-#define nomboflines 20
-	int Xnomboflines=0;
-	int Ynomboflines=0;
-	int  fld[nomboflines+1][nomboflines+1]; 
-	int TopL[nomboflines][(nomboflines/2)+(nomboflines%2)];
-	int	LeftL[nomboflines][(nomboflines/2)+(nomboflines%2)];	
-void startpos(int Line)
-{
-	int nomb=0;
-	int k1=0,k2=0,x=0;	
-	for (k1=0;k1<Xnomboflines;k1++)
-		fld[Line][k1]=0;
-	while ((LeftL[Line][nomb]!=0) && (nomb<((Xnomboflines/2)+(Xnomboflines%2))))
-		nomb++;	
-	for (k1=0;k1<nomb;k1++){
-		for (k2=1;k2<=LeftL[Line][k1];k2++){
+#include <stdio_ext.h>
+#include <stdlib.h>
+#define numboflines 50
+	int  fld[numboflines+1][numboflines+1]; 
+	int TopL[numboflines][(numboflines/2)+(numboflines%2)];
+	int	LeftL[numboflines][(numboflines/2)+(numboflines%2)];
+	
+void startpos(int Line,int Xnumboflines)
+{	
+	int numb=0;
+	int i=0,j=0,x=0;	
+	for (i=0;i<Xnumboflines;i++)
+		fld[Line][i]=0;
+	while ((LeftL[Line][numb]!=0) && (numb<((Xnumboflines/2)+(Xnumboflines%2))))
+		numb++;	
+	for (i=0;i<numb;i++){
+		for (j=1;j<=LeftL[Line][i];j++){
 			fld[Line][x]=1;
 			x++;
 		}			
 	x++;
 	}		
 }
-int chngelm(int Line,int elem)
-{
-	int k1;
+int chngelm(int Line,int elem,int Xnumboflines)
+{	
+	int i;
 	int x=-1,take=1,take1=0;
-	for (k1=0;k1<elem-1;k1++)
-		take=take+LeftL[Line][k1];	
-	for (k1=0;k1<Xnomboflines;k1++){
+	for (i=0;i<elem-1;i++)
+		take=take+LeftL[Line][i];	
+	for (i=0;i<Xnumboflines;i++){
 		x++;
-		take1=take1+fld[Line][k1];
+		take1=take1+fld[Line][i];
 		if (take1==take)
-			k1=Xnomboflines;	
-	}
-	
-	if ((fld[Line][x+LeftL[Line][elem-1]+1]!=0) || (x+LeftL[Line][elem-1]-1==Xnomboflines-1)){
+			i=Xnumboflines;	
+	}	
+	if ((fld[Line][x+LeftL[Line][elem-1]+1]!=0) || (x+LeftL[Line][elem-1]-1==Xnumboflines-1)){
 		return 1;
 	}
 	else{
@@ -43,259 +43,229 @@ int chngelm(int Line,int elem)
 		return 0;
 	}	
 }
-int returnelm(int Line,int elem)
+int returnelm(int Line,int elem,int Xnumboflines)
 {
 	int x1=-1,x2=-1,btake=1,btake1=0,atake=1,atake1=0;
-	int k1;
-	for (k1=0;k1<elem-1;k1++)
-			btake=btake+LeftL[Line][k1];	
-		for (k1=0;k1<Xnomboflines;k1++){
-			x2++;
-			btake1=btake1+fld[Line][k1];
-			if (btake1==btake)
-				k1=Xnomboflines;	
-		}
-		for (k1=0;k1<elem-2;k1++)
-			atake=atake+LeftL[Line][k1];	
-		for (k1=0;k1<Xnomboflines;k1++){
-			x1++;
-			atake1=atake1+fld[Line][k1];
-			if (atake1==atake)
-				k1=Xnomboflines;	
-		}		
-		if (x2==x1+LeftL[Line][elem-2]+1)
-			return 1;
-		else{
-			for (k1=x2;k1<x2+LeftL[Line][elem-1];k1++)
-				fld[Line][k1]=0;
-			for (k1=x1+LeftL[Line][elem-2]+1;k1<=x1+LeftL[Line][elem-2]+LeftL[Line][elem-1];k1++)
-				fld[Line][k1]=1;					
-			return 0;
-		}
-}
-int returnchnge(int Line,int elem)
-{
-	int n=0,x1=-1,x2=-1,btake=1,btake1=0,atake=1,atake1=0,nomb=0;
-	int k1;
-	while ((LeftL[Line][nomb]!=0) && (nomb<((Xnomboflines/2)+(Xnomboflines%2))))
-		nomb++;
-	n=chngelm(Line,elem);
-	if (n==1)
+	int i;
+	for (i=0;i<elem-1;i++)
+			btake=btake+LeftL[Line][i];	
+	for (i=0;i<Xnumboflines;i++){
+		x2++;
+		btake1=btake1+fld[Line][i];
+		if (btake1==btake)
+			i=Xnumboflines;	
+	}
+	for (i=0;i<elem-2;i++)
+		atake=atake+LeftL[Line][i];	
+	for (i=0;i<Xnumboflines;i++){
+		x1++;
+		atake1=atake1+fld[Line][i];
+		if (atake1==atake)
+			i=Xnumboflines;	
+	}		
+	if (x2==x1+LeftL[Line][elem-2]+1)
 		return 1;
 	else{
-		for (k1=0;k1<elem;k1++)
-			btake=btake+LeftL[Line][k1];	
-		for (k1=0;k1<Xnomboflines;k1++){
-			x2++;
-			btake1=btake1+fld[Line][k1];
-			if (btake1==btake)
-				k1=Xnomboflines;	
-		}
-		for (k1=0;k1<elem-1;k1++)
-			atake=atake+LeftL[Line][k1];	
-		for (k1=0;k1<Xnomboflines;k1++){
-			x1++;
-			atake1=atake1+fld[Line][k1];
-			if (atake1==atake)
-				k1=Xnomboflines;	
-		}		
-		if (x2==x1+LeftL[Line][elem-1]+1)
-			if (elem==1)
-				return 3;
-			else
-				return 2;
-		
+		for (i=x2;i<x2+LeftL[Line][elem-1];i++)
+			fld[Line][i]=0;
+		for (i=x1+LeftL[Line][elem-2]+1;i<=x1+LeftL[Line][elem-2]+LeftL[Line][elem-1];i++)
+			fld[Line][i]=1;					
+		return 0;
+	}
+}
+int returnchnge(int Line,int elem,int numb,int Xnumboflines)
+{	
+	int n=0,r;
+	int i;
+	n=chngelm(Line,elem,Xnumboflines);
+	if (n==1)
+		return 2;
+	else{
+		r=returnelm(Line,elem+1,Xnumboflines);		
+		if (r==1)
+			return 1;
 		else{
-			for (k1=x2;k1<x2+LeftL[Line][elem];k1++)
-				fld[Line][k1]=0;
-			for (k1=x1+LeftL[Line][elem-1]+1;k1<=x1+LeftL[Line][elem-1]+LeftL[Line][elem];k1++)
-				fld[Line][k1]=1;
-			for (k1=elem+2;k1<=nomb;k1++)
-				returnelm(Line,k1);
+			for (i=elem+2;i<=numb;i++)
+				returnelm(Line,i,Xnumboflines);
 			return 0;
 		}	
 	}
 }
 
-int chngpos(int Line)
-{
-	int nomb=0,all=0,elem;
-	int k1,k2,n=0,k=1;
-	while ((LeftL[Line][nomb]!=0) && (nomb<((Xnomboflines/2)+(Xnomboflines%2))))
-		nomb++;
-	elem=nomb;
-	for (k1=0;k1<nomb;k1++)
-		all=all+LeftL[Line][k1];
-	all=all+nomb-1;
-	if (all==Xnomboflines)
+int chngpos(int Line,int Xnumboflines)
+{	
+	int numb=0,all=0,elem;
+	int i,n=0,k=3;
+	while ((LeftL[Line][numb]!=0) && (numb<((Xnumboflines/2)+(Xnumboflines%2))))
+		numb++;
+	elem=numb;
+	for (i=0;i<numb;i++)
+		all=all+LeftL[Line][i];
+	all=all+numb-1;
+	if (all==Xnumboflines || all==-1)
 		return 1;
 	else{
-		n=chngelm(Line,elem);
-		if (n==1){
-			k=n;
-			k2=0;
-			while (k!=0 && k!=3){
-				k2++;
-				k=returnchnge(Line,elem-k2);
+		n=chngelm(Line,numb,Xnumboflines);
+		if (n==1 && numb!=1){
+			while (k!=0 && k!=1 && elem>1){
+				elem--;
+				k=returnchnge(Line,elem,numb,Xnumboflines);
 			}
 		}
-		if (k==3)
+		if ((k==2 && elem==1) || (n==1 && numb==1))
 			return 2;
 		else
 			return 0;
-	}	
+	}
 }
-int filt1(int Clmn,int Nmb)
-{
-	int k1,n=0;
-	for (k1=0;k1<=Nmb;k1++)
-		if (fld[k1][Clmn]!=fld[Ynomboflines][k1]){
-			k1=Ynomboflines;
+int filt1(int Clmn,int Nmb,int Ynumboflines)
+{	
+	int i,n=0;
+	for (i=0;i<=Nmb;i++){ 
+		if (fld[i][Clmn]!=fld[Ynumboflines][i]){
+			i=Nmb+1;
 			n=1;
 		}
+	} 
 	return n;
 }
-int filt(int Line)
+int filt(int Line,int Xnumboflines,int Ynumboflines)
 {
 	int k=0,p=1,t;
-	int k1,Column;
-	for (Column=0;Column<Xnomboflines;Column++){
-		k1=0;
-		while (TopL[Column][k1]!=0){//заполняет левую колонку
-			LeftL[Ynomboflines][k1]=TopL[Column][k1];
-			k1++;
+	int i,j,Column;
+	for (Column=0;Column<Xnumboflines;Column++){
+		i=0;
+		while (TopL[Column][i]!=0){
+			LeftL[Ynumboflines][i]=TopL[Column][i];
+			i++;
 		}
-		startpos(Ynomboflines);
-		p=filt1(Column,Line);
+		startpos(Ynumboflines,Ynumboflines);
+		p=filt1(Column,Line,Ynumboflines);
 		while(k!=2 && k!=1 && p!=0){
-			k=chngpos(Ynomboflines);
-			p=filt1(Column,Line);
+			k=chngpos(Ynumboflines,Ynumboflines);
+			p=filt1(Column,Line,Ynumboflines);
 		}
+		for (j=0;j<=i;j++)
+			LeftL[Ynumboflines][j]=0;
 		if (p==0)
 			t=0;
 		else {
 			t=1;
-			Column=Xnomboflines;
+			Column=Xnumboflines;
 		}
 	}
 	return	t;
 }
-/*
-int push(){
-	int k=0,p=0;
-	int k1;
-	for (k1=0;k1<Ynomboflines;k1++)
-		startpos(k1);
-	for (k1=0;k1<Ynomboflines;k1++){
-		p=filt(k1);
+
+int push(int Xnumboflines,int Ynumboflines)
+{
+	int k=0,p=0,t=2;
+	int i;
+	for (i=0;i<Ynumboflines;i++)
+		startpos(i,Xnumboflines);
+			
+	for (i=0;i<Ynumboflines;i++){ 
+		p=filt(i,Xnumboflines,Ynumboflines);	
 		while (k!=2 && k!=1 && p!=0){
-			k=chngpos(k1);
-			p=filt(k1);
+			k=chngpos(i,Xnumboflines);
+			p=filt(i,Xnumboflines,Ynumboflines);
 		}
 		if (p==0){
 			t=0;
 			k=0;
 		}
 		else{
-			if (k1==0)
-				k1=Ynomboflines;
-				t=1;
-			else{
-				
-				k=1;
-				while(k==1){
-					k1--;
-					k=changepos(k1)
-				
-				}
-				
+			k=1;
+			while((k==1 || k==2) && i>0){
+				startpos(i,Xnumboflines);
+				k=chngpos(i-1,Xnumboflines);
+				i--;
+			}
+			i--;
+			if (i<0 && k==1){
+				exit(1);
 			}
 		}
-	}	
+	}
+	return t;	
 }
-*/
+
 int main()
 {	
-	int nombofelements=(nomboflines/2)+(nomboflines%2);
-	int k1,k2,c,k;
-	int Xnombofelements=0,Ynombofelements=0;
-	for (k1=0;k1<nomboflines;k1++)
-		for (k2=0;k2<nomboflines;k2++)
-			fld[k1][k2]=0;
-	for (k1=0;k1<nomboflines;k1++)
-		for (k2=0;k2<=nombofelements-1;k2++){
-			LeftL[k1][k2]=0;
-			TopL[k1][k2]=0;
+	int i,j,c,k;
+	int Xnumboflines=0;
+	int Ynumboflines=0;
+	for (i=0;i<numboflines;i++)
+		for (j=0;j<numboflines;j++)
+			fld[i][j]=0;
+	for (i=0;i<numboflines;i++)
+		for (j=0;j<=(numboflines/2)+(numboflines%2)-1;j++){
+			LeftL[i][j]=0;
+			TopL[i][j]=0;
 		}
 	printf("\nEnter size (x and y)\n");
 	while ((c=getchar())!=(int)'\n')
-		Xnomboflines=Xnomboflines*10+c-(int)'0';
+		Xnumboflines=Xnumboflines*10+c-(int)'0';
 	while ((c=getchar())!=(int)'\n')
-		Ynomboflines=Ynomboflines*10+c-(int)'0';
-	Xnombofelements=(Xnomboflines/2)+(Xnomboflines%2);
-	Ynombofelements=(Ynomboflines/2)+(Ynomboflines%2);		
-	printf("//%d//%d//",Xnombofelements,Ynombofelements);
-	printf("Enter Top Line:");				
-	for (k1=0;k1<Xnomboflines;k1++){
-		printf("\nColumn %d:  ",k1+1);
-		for (k2=0;k2<Ynombofelements;k2++){
-			printf("\nNmb %d:  ",k2+1);
-			while ((c=getchar())!=(int)'\n'){
-					TopL[k1][k2]=TopL[k1][k2]*10+c-(int)'0';
+		Ynumboflines=Ynumboflines*10+c-(int)'0';
+	printf("Enter Top Line(%d elements):",(Ynumboflines/2)+(Ynumboflines%2));
+	__fpurge(stdin);				
+	for (i=0;i<Xnumboflines;i++){
+		printf("\nColumn %d:  ",i+1);
+		for (j=0;j<=(Ynumboflines/2)+(Ynumboflines%2);j++){
+			(void)scanf("%d",&TopL[i][j]);
+			if (TopL[i][j]==0){printf("%d/%d",i,j);
+				j=(Ynumboflines/2)+(Ynumboflines%2);
+				__fpurge(stdin);
 			}
-			if (TopL[k1][k2]==0)
-				k2=Ynombofelements;
-			printf("\n");
-		}
-	}	
-	printf("\nEnter Left Column:");	
-	for (k1=0;k1<Ynomboflines;k1++){
-		printf("\nLine %d: ",k1+1);	
-		for (k2=0;k2<Xnombofelements;k2++){
-			printf("\nNmb %d:  ",k2+1);
-			while ((c=getchar())!=(int)'\n'){
-					LeftL[k1][k2]=LeftL[k1][k2]*10+c-(int)'0';
-				}
-			if (LeftL[k1][k2]==0)
-				k2=Xnombofelements;
+			else
+				(void)scanf(",");
 		}
 	}
+		
+	printf("\nEnter Left Column(%d elements):",(Xnumboflines/2)+(Xnumboflines%2));
+		
+	for (i=0;i<Ynumboflines;i++){
+		printf("\nLine %d: ",i+1);	
+		for (j=0;j<=(Xnumboflines/2)+(Xnumboflines%2);j++){
+			scanf("%d,",&LeftL[i][j]);
+			if (LeftL[i][j]==0){
+				j=(Xnumboflines/2)+(Xnumboflines%2);
+				__fpurge(stdin);
+			}
+			else 
+				(void)scanf(",");
+		}
+	}
+	
 	printf("Top LIne:");
-	for (k1=0;k1<Ynombofelements;k1++){
+	
+	for (i=0;i<(Ynumboflines/2)+(Ynumboflines%2);i++){
 		printf("\n");
-		for (k2=0;k2<Xnomboflines;k2++)
-			printf("%d/",TopL[k2][k1]);
+		for (j=0;j<Xnumboflines;j++)
+			printf("%3d/",TopL[j][i]);
 	}
+	
 	printf("\nLeft Line:");
-	for (k1=0;k1<Ynomboflines;k1++){
+	for (i=0;i<Ynumboflines;i++){
 		printf("\n");
-		for (k2=0;k2<Xnombofelements;k2++)
-			printf("%d/",LeftL[k1][k2]);
-	}			
-	/*/------------------------------------------------------------------///Start//*/
-	startpos(0);
-	startpos(1);
-	startpos(2);
-	startpos(3);
-	startpos(4);
-	while (k!=2 && k!=1) {
-		k=chngpos(0);
-		for (k1=0;k1<Ynomboflines;k1++){
+		for (j=0;j<(Xnumboflines/2)+(Xnumboflines%2);j++)
+			printf("%3d/",LeftL[i][j]);
+	}
+
+	k=push(Xnumboflines,Ynumboflines);
+
+	if (k==0)
+		for (i=0;i<Ynumboflines;i++){
 			printf("\n");
-			for (k2=0;k2<Xnomboflines;k2++)
-				printf("%d/",fld[k1][k2]);
+			for (j=0;j<Xnumboflines;j++)
+				if (fld[i][j]==1)
+					printf("██");
+				else 
+					printf("  ");
 		}
-		printf("\n");
-	}
-	k=filt(4);
-	printf("filt: /%d/\n",k);
-	
-	
-	/*/------------------------------------------------------------------///End//*/
-	for (k1=0;k1<Ynomboflines;k1++){
-		printf("\n");
-		for (k2=0;k2<Xnomboflines;k2++)
-			printf("%d/",fld[k1][k2]);
-	}
+	else
+		printf("\nCan't do it!!!!!!!!!!!!!!!((((((\n");
+		
 	return 0;
 }
